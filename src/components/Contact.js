c65e8b7f2dee3c5d66cae3a66ed6db2a70a58702
Mark Timothy Advento <mark@adventodesign.com>
2022-03-19 04:52:19 +0800

    Update contact.js
    Wrong number

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -59,6 +59,7 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
+              <p className="leading-relaxed">+63-977-801-0897</p>
             </div>
           </div>
         </div>
